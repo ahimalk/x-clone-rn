bde35f3b12c9a88ecee969a6d85fd3c559532b7e
ahimalk <ahimalkrishna@gmail.com>
2025-07-23 15:10:28 +0530

    user synced to db

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,15 +2,20 @@ import { Stack } from "expo-router";
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import "../global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+
+const queryClinet = new QueryClient();
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache} >
+      <QueryClientProvider client={queryClinet}>
       <Stack screenOptions={{headerShown: false}}>
       <Stack.Screen name="(auth)" />
       <Stack.Screen name="(tabs)" />
       </Stack>
+      </QueryClientProvider>
     </ClerkProvider>
   )
 }
